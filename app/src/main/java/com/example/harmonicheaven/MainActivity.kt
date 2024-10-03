ef75ef6838dae72611eb160cea892417fb392c20
Umayangana Wijayasiri <it21244452@my.sliit.lk>
2024-10-03 20:41:13 +0530

    First test run is done
    Recycler view created. Database is connected for sure

--- app/src/main/java/com/example/harmonicheaven/MainActivity.kt
+++ app/src/main/java/com/example/harmonicheaven/MainActivity.kt
@@ -5,17 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.harmonicheaven.adapter.CategoryAdapter
+import com.example.harmonicheaven.databinding.ActivityMainBinding
+import com.example.harmonicheaven.models.categoryModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var categoryAdapter: CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        getCategories()
+
     }
+
+    fun getCategories(){
+        FirebaseFirestore.getInstance().collection("category")
+            .get().addOnSuccessListener {
+                val categoryList = it.toObjects(categoryModel::class.java)
+                setupCategoryRecyclerView(categoryList)
+            }
+    }
+
+    fun setupCategoryRecyclerView(categoryList : List<categoryModel>){
+        categoryAdapter = CategoryAdapter(categoryList)
+        binding.categoriesRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        binding.categoriesRecyclerView.adapter = categoryAdapter
+
+    }
+
+
 }
