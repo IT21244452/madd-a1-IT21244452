b93766ffc84a0b05a792a3bb3a95e7ecf0d519f7
Umayangana Wijayasiri <it21244452@my.sliit.lk>
2024-10-03 20:03:44 +0530

    Firebase configured

---
+++ app/src/main/java/com/example/harmonicheaven/adapter/CategoryAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.harmonicheaven.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.harmonicheaven.databinding.CategoryItemRecyclerRowBinding
+import com.example.harmonicheaven.models.categoryModel
+
+class CategoryAdapter (private val categoryList: List<categoryModel>) :
+    RecyclerView.Adapter<CategoryAdapter.MyViewHolder>() {
+
+    class MyViewHolder(private val binding : CategoryItemRecyclerRowBinding) :
+        RecyclerView.ViewHolder(binding.root){
+            //binding the data with views
+            fun bindData(category : categoryModel){
+                binding.nameTextView.text = category.name
+
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = CategoryItemRecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindData(categoryList[position])
+    }
+
+}
---
+++ app/src/main/java/com/example/harmonicheaven/models/categoryModel.kt
@@ -0,0 +1,8 @@
+package com.example.harmonicheaven.models
+
+data class categoryModel(
+    val name : String,
+    val coverUrl : String,
+){
+    constructor() : this("","")
+}
